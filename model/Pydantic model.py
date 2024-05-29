b6b285614b232eedfb31d14a4eec4393d7bb6a94
Raghad Alharbi <Raghad.alharbi02@gmail.com>
2024-05-29 14:25:02 +0300

    R

---
+++ model/Pydantic model.py
@@ -0,0 +1,9 @@
+
+class InputFeatures(BaseModel):
+    age: float
+    appearance: int
+    minutes_played: int
+    days_injured: int
+    games_injured: int
+    award: int
+    current_value: int
